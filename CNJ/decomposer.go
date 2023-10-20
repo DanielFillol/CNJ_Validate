8484a7c86654201f3b2702214ca598e4f025e4b3
daniel_fillol@hotmail.com <55287657+DanielFillol@users.noreply.github.com>
2023-10-19 21:20:20 -0300

    feat: add another case of special TJ

--- CNJ/decomposer.go
+++ CNJ/decomposer.go
@@ -62,7 +62,7 @@ func DecomposeCNJ(cnj string) (DecomposedCNJ, error) {
 		}
 
 		var tj string
-		if sg.Number == 1 || sg.Number == 2 || sg.Number == 3 || sg.Number == 7 {
+		if sg.Number == 1 || sg.Number == 2 || sg.Number == 3 || sg.Number == 4 || sg.Number == 7 {
 			courtNumber, err := strconv.Atoi(court)
 			if err != nil {
 				return DecomposedCNJ{
@@ -137,7 +137,7 @@ func DecomposeCNJ(cnj string) (DecomposedCNJ, error) {
 		}
 
 		var tj string
-		if sg.Number == 1 || sg.Number == 2 || sg.Number == 3 || sg.Number == 7 {
+		if sg.Number == 1 || sg.Number == 2 || sg.Number == 3 || sg.Number == 4 || sg.Number == 7 {
 			courtNumber, err := strconv.Atoi(court)
 			if err != nil {
 				return DecomposedCNJ{
